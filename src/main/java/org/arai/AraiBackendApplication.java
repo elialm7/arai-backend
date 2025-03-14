733928ddcc6e6416013c150aa588955db7563691
elialm7 <metall65eli@gmail.com>
2025-03-13 21:44:16 -0300

    initial commit

---
+++ src/main/java/org/arai/AraiBackendApplication.java
@@ -0,0 +1,15 @@
+package org.arai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AraiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AraiBackendApplication.class, args);  
+                
+                
+	}
+
+}
---
+++ src/test/java/org/arai/AraiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.arai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AraiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
