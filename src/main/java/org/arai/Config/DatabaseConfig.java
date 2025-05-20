aa30240201b6f35eb926b3b1623c774f03493e86
elialm7 <metall65eli@gmail.com>
2025-05-20 01:28:28 -0300

    chore: udpates

--- src/main/java/org/arai/Config/DatabaseConfig.java
+++ src/main/java/org/arai/Config/DatabaseConfig.java
@@ -1,6 +1,7 @@
 package org.arai.Config;
 
 import com.zaxxer.hikari.HikariDataSource;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +12,7 @@ import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
+import java.sql.SQLException;
 
 @Configuration
 @EnableTransactionManagement
@@ -39,10 +41,12 @@ public class DatabaseConfig {
         dataSource.setConnectionTimeout(30000);
         dataSource.setIdleTimeout(600000);
         dataSource.setMaxLifetime(1800000);
-        dataSource.setConnectionTestQuery("SELECT 1");
         dataSource.setInitializationFailTimeout(30000);
+
         return dataSource;
     }
+
+
 
 
     @Bean
