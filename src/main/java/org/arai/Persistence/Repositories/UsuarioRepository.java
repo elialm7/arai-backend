b74e4a3b18044174591b2a6bc8145d2ecc08c30a
elialm7 <metall65eli@gmail.com>
2025-06-11 15:34:22 -0300

    chore: repository de usuario y test de usuario

--- src/main/java/org/arai/Persistence/Repositories/UsuarioRepository.java
+++ src/main/java/org/arai/Persistence/Repositories/UsuarioRepository.java
@@ -2,17 +2,23 @@ package org.arai.Persistence.Repositories;
 
 import org.arai.Persistence.Entities.Usuario;
 import org.arai.Exceptions.UsuarioNoEncontradoException;
+import org.arai.Persistence.QueryResults.UsuarioPermisoResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.xml.crypto.Data;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class UsuarioRepository {
@@ -37,6 +43,20 @@ public class UsuarioRepository {
 
     }
 
+    private Optional<UsuarioPermisoResult> mapToUsuarioPermisoResult(ResultSet resultSet, int i) throws SQLException {
+        var usuarioPermisoResult = new UsuarioPermisoResult();
+        usuarioPermisoResult.setId_usuario(resultSet.getInt("usuario_id"));
+        usuarioPermisoResult.setNombreRol(resultSet.getString("rol_nombre"));
+        usuarioPermisoResult.setPermisos(Arrays.asList((String[])resultSet.getArray("permisos").getArray()));
+        return Optional.of(usuarioPermisoResult);
+    }
+
+    /**
+     * Recupera todos los usuarios de la base de datos.
+     *
+     * @return Una lista de objetos `Usuario` que contiene los datos de todos los usuarios.
+     *         Si ocurre un error al acceder a la base de datos, se devuelve una lista vacía.
+     */
     @Transactional(readOnly = true)
     public List<Usuario> findAllUsuarios() {
         List<Usuario> usuarios = new ArrayList<>();;
@@ -44,23 +64,41 @@ public class UsuarioRepository {
             String sql = "select * from tb_usuarios";
             usuarios = jdbcTemplate.query(sql, (rs, rowNum) -> mapToUsuario(rs));
         } catch (DataAccessException e) {
-            log.error(e.getMessage());
+            log.error("Error al recuperar usuarios de la base de datos", e);
         }
         return usuarios;
     }
 
+    /**
+     * Busca un usuario en la base de datos utilizando su cédula como criterio de búsqueda.
+     *
+     * @param cedula La cédula del usuario que se desea buscar.
+     * @return Un objeto `Usuario` que contiene los datos del usuario encontrado.
+     * @throws UsuarioNoEncontradoException Si no se encuentra un usuario con la cédula proporcionada
+     *                                      o si ocurre un error al acceder a la base de datos.
+     */
     @Transactional(readOnly = true)
     public Usuario findUsuarioByCedula(String cedula) throws UsuarioNoEncontradoException {
         try {
             String sql = "SELECT * FROM tb_usuarios WHERE cedula = ?;";
             return jdbcTemplate.queryForObject(sql, (rs, rownum) -> mapToUsuario(rs), cedula);
         } catch (DataAccessException e) {
+            log.error("Error al buscar usuario por cedula: {}", cedula, e);
             throw new UsuarioNoEncontradoException("No se encontro el usuario", e);
         }
     }
 
+    /**
+     * Actualiza los datos de un usuario en la base de datos.
+     *
+     * @param usuario Un objeto `Usuario` que contiene los datos actualizados del usuario.
+     *                Los campos requeridos son: `id_user`, `id_rol_fk`, `apellido`, `correo`,
+     *                `nombre`, `password` y `username`.
+     * @return Un entero que indica el número de filas afectadas por la operación de actualización.
+     *         Devuelve -1 si ocurre un error durante la operación.
+     */
     @Transactional()
-    public Integer updateUsuarioById(Usuario usuario){
+    public Integer updateUsuario(Usuario usuario){
         try {
             String sql = """
                     UPDATE tb_usuarios
@@ -84,40 +122,78 @@ public class UsuarioRepository {
     }
 
 
+    /**
+     * Inserta un nuevo usuario en la base de datos.
+     *
+     * @param usuario Un objeto `Usuario` que contiene los datos del usuario a insertar.
+     *                Los campos requeridos son: `id_rol_fk`, `apellido`, `correo`, `nombre`,
+     *                `password`, `username` y `cedula`.
+     * @return El identificador único (`id_user`) del usuario insertado si la operación es exitosa,
+     *         o -1 si ocurre un error durante la inserción.
+     */
     @Transactional
-    public void createUsuario(Usuario usuario) {
+    public Integer insertUsuario(Usuario usuario) {
+        KeyHolder keyHolder = new GeneratedKeyHolder();
         try {
                 String sql = """
-                    
                         INSERT INTO tb_usuarios(
                         id_rol_fk,
-                        id_user, 
-                        apellido, 
-                        correo, 
+                        apellido,
+                        correo,
                         nombre,
-                        password, 
-                        username, 
-                        cedula)
-                        VALUES (?, ?, ?, ?, ?, ?, ?, ?);
-       
-    
+                        password,
+                        username,
+                        cedula)VALUES (?,  ?, ?, ?, ?, ?, ?);
                    """;
 
-            jdbcTemplate.update(sql,
-                    usuario.getId_rol_fk(),
-                    usuario.getId_user(),
-                    usuario.getApellido(),
-                    usuario.getCorreo(),
-                    usuario.getNombre(),
-                    usuario.getPassword(),
-                    usuario.getUsername(),
-                    usuario.
-                getCedula()
+            jdbcTemplate.update(con -> {
+                    var ps = con.prepareStatement(sql, new String[]{"id_user"});
+                    ps.setInt(1, usuario.getId_rol_fk());
+                    ps.setString(2, usuario.getApellido());
+                    ps.setString(3, usuario.getCorreo());
+                    ps.setString(4, usuario.getNombre());
+                    ps.setString(5, usuario.getPassword());
+                    ps.setString(6, usuario.getUsername());
+                    ps.setString(7, usuario.getCedula());
+                    return ps;
+                }, keyHolder
             );
+            return keyHolder.getKey() != null ? keyHolder.getKey().intValue() : -1;
         } catch (Exception e) {
             log.error(e.getMessage());
+            return -1;
         }
 
+    }
+
+    /**
+     * Recupera los datos de permisos asociados a un usuario específico.
+     *
+     * @param user_id El identificador único del usuario para el cual se desean obtener los datos de permisos.
+     * @return Un objeto `Optional<UsuarioPermisoResult>` que contiene los datos del usuario, su rol y los permisos asociados.
+     *         Si ocurre un error o no se encuentran datos, se devuelve un `Optional.empty()`.
+     */
+    @Transactional(readOnly = true)
+    public Optional<UsuarioPermisoResult> getUsuarioPermisoData(Integer user_id){
+        try {
+
+            String sql = """
+                    SELECT
+                        u.id_user  as usuario_id,
+                        r.nombre_rol as rol_nombre,
+                        array_agg(p.nombre_permiso ORDER BY p.nombre_permiso) as permisos
+                    FROM tb_usuarios u
+                    INNER JOIN tb_roles r ON r.id_rol = u.id_rol_fk
+                    INNER JOIN tb_rol_permisos rp ON rp.id_rol_fk = r.id_rol
+                    INNER JOIN tb_permisos p ON  p.id_permiso = rp.id_permiso_fk
+                    WHERE  u.id_user = ?
+                    GROUP BY u.id_user, r.nombre_rol;
+                    """;
+            return jdbcTemplate.queryForObject(sql, this::mapToUsuarioPermisoResult, user_id);
+        }catch (DataAccessException e){
+            log.error("Error al obtener datos de usuario y permisos: {}", e.getMessage());
+            return Optional.empty();
+        }
     }
 
 
